6620ad6db18971f8e58c5ea81898895f3c88b021
unknown <riyamittal34@gmail.com>
2022-04-30 16:26:24 +0530

    Adding dependency for Eureka Client

--- src/main/java/com/stock/microservice/MicroserviceApplication.java
+++ src/main/java/com/stock/microservice/MicroserviceApplication.java
@@ -2,6 +2,7 @@ package com.stock.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
  */
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableEurekaClient
 public class MicroserviceApplication {
 
 	/**
