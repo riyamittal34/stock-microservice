1271647da840ea1c6c02a743e3345bef2899f322
unknown <riyamittal34@gmail.com>
2022-05-14 01:26:40 +0530

    Removing bean

--- src/main/java/com/stock/microservice/MicroserviceApplication.java
+++ src/main/java/com/stock/microservice/MicroserviceApplication.java
@@ -1,10 +1,8 @@
 package com.stock.microservice;
 
-import org.springdoc.core.GroupedOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
@@ -25,15 +23,5 @@ public class MicroserviceApplication {
 	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MicroserviceApplication.class, args);
-	}
-
-	/**
-	 * Api.
-	 *
-	 * @return the grouped open api
-	 */
-	@Bean
-	public GroupedOpenApi api() {
-		return GroupedOpenApi.builder().group("StockController").packagesToScan("com.stock.microcservice").build();
 	}
 }
